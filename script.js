e5cc6a6a50df57282047abcba0c18358574a9895
Divyansh <92048020+Divi03@users.noreply.github.com>
2022-02-28 23:55:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function showWeatherData(data){
     data.daily.forEach((day,idx) =>{
         if(idx == 0){
             currentTempEl.innerHTML = `
-            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png" alt="weather icon" class="w-icon">
+            <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}@4x.png" alt="weather icon" class="w-icon">
             <div class="other">
                 <div class="day">${window.moment(day.dt*1000).format('ddd')}</div>
             <div class="temp">Night - ${day.temp.night}&#176; C</div>
